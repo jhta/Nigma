01d1c19796ef87ad6872b80bdd11234d257a5519
Jonathan Vallejo Muñoz <jonathanvm13@gmail.com>
2015-07-14 02:06:51 -0500

    sign in  & sign up added

---
+++ js/user.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  var url = "http://localhost:4000/api";
+
+  //Sign up
+  $("#signUp").click(function (e) {
+    e.preventDefault();
+    var data = {
+      user: {
+        email: $("#signUp_email").val(),
+        name: $("#signUp_name").val(),
+        pass: $("#signUp_password").val()
+      }
+    };
+
+    $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url: url + "/users",
+      data: JSON.stringify(data)
+    }).done(function (data) {
+      $(location).attr('href', 'http://localhost:8000');
+    })
+      .fail(function (data) {
+        alert("Datos inválidos, intenta de nuevo");
+      });
+  });
+
+  //Sign In
+  $("#signIn").click(function (e) {
+    e.preventDefault();
+    var data = {
+      user: {
+        email: $("#signIn_email").val(),
+        pass: $("#signIn_password").val()
+      }
+    };
+
+    $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url: url + "/users/login",
+      data: JSON.stringify(data)
+    })
+      .done(function (data) {
+        $(location).attr('href', 'http://localhost:8000');
+      })
+      .fail(function (data) {
+        alert("Correo electrónico o password incorrecto");
+      });
+  });
+
+});
