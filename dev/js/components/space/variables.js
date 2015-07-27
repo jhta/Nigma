988ac6440a40ea8aa8279e6859b2049150b581fe
jhta <jeisonhs93@gmail.com>
2015-07-27 11:08:48 -0500

    variables Modal

--- dev/js/components/space/variables.js
+++ dev/js/components/space/variables.js
@@ -1,13 +1,34 @@
 const React = require("react");
 
+//USED
+const Modal = require('./../util/modal');
+
+
 const Variables = React.createClass({
+
+  getInitialState() {
+    return {
+      showModal: false,
+      typeOfVariable: 0
+    }
+  },
+
+  _openModal(type) {
+    console.log("0pen");
+    this.setState({showModal: true, typeOfVariable: type});
+  },
 
   render() {
     return (
       <div className="Space-right z-depth-1">
         <div className="Variables">
           <Variables.Header />
-          <Variables.Content />
+          <Variables.Content
+            onOpenModal = {this._openModal}
+          />
+          <Variables.Modal
+            showModal = {this.state.showModal}
+          />
         </div>
     </div>
     );
@@ -16,6 +37,7 @@ const Variables = React.createClass({
 });
 
 Variables.Header = React.createClass({
+
   render() {
     return (
       <nav className="Variables-Header teal">
@@ -30,18 +52,24 @@ Variables.Header = React.createClass({
 });
 
 Variables.Content = React.createClass({
-
   render() {
     return (
       <div className="Variables-Content">
         Nothing here yet...
-        <Variables.Dropdown />
+        <Variables.Dropdown {...this.props}/>
       </div>
     )
   }
 });
 
 Variables.Dropdown = React.createClass({
+
+  _selectType(type) {
+    return ()=>{
+      this.props.onOpenModal(type);
+    }
+  },
+
   render() {
     return (
       <div>
@@ -51,13 +79,63 @@ Variables.Dropdown = React.createClass({
         </div>
         {/* Dropdown Structure */}
         <ul id="dropdown-variables" className="dropdown-content">
-          <li><a href="#!">one</a></li>
-          <li><a href="#!">two</a></li>
-          <li><a href="#!">three</a></li>
+          <li>
+            <a
+              onClick = {this._selectType(0)}
+              href="javascript:void(0)"
+            >
+              one
+            </a>
+          </li>
+          <li><a href="javascript:void(0)">two</a></li>
+          <li><a href="javascript:void(0)">three</a></li>
         </ul>
       </div>
 
     );
+  }
+});
+
+Variables.Modal = React.createClass({
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.showModal) {
+      this._openModal();
+    }
+  },
+
+  _openModal() {
+    this.refs.modal.openModal();
+  },
+
+  _selectType() {
+    let type = this.props.type;
+    switch(type) {
+      case 0:
+        console.log("0");
+        break;
+
+      case 1:
+        console.log("0");
+        break;
+    }
+  },
+
+  render() {
+    return (
+      <Modal
+        headerText="Nueva pregunta"
+        ref="modal"
+        positiveCallback={console.log("holi")}
+      >
+        <div className="input-field col s12">
+          <input
+            type="text"
+          />
+          <label htmlFor="question-name">Nombre</label>
+        </div>
+      </Modal>
+    )
   }
 });
 
