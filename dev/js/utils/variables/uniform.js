c1282f1f4dac12cc4b15c845f483f39341a445ab
Alexander Soto <nickforspeed25@gmail.com>
2015-08-04 23:05:55 -0500

    Created Model VariableUniform
    * Methods are checkSyntax, generateCode, getParameters and its constructor

--- dev/js/utils/variables/uniform.js
+++ dev/js/utils/variables/uniform.js
@@ -1,46 +1,75 @@
-const uniform =  {
-  name: 'uniform',
-  identifier: 'U',
-  syntax: /([a-zA-Z\_\-]+[0-9A-Za-z\_\-\$]*)*\s*=\s*(u|U)\[([^\;]+)\;([^\;]+)\;([^\]]+)\]/,
-  checkSyntax: function (nigmaCode) {
-    var regex = this.syntax;
-    var match = nigmaCode.match(regex);
-    if(match && [match[3].trim(), match[4].trim(), match[5].trim()].every(function (element) {
-      return element != ''
-    })){
+var Uniform = function(codeFragment) {
+  var self = this;
+  self.codeFragment = codeFragment;
+
+  self.checkSyntax = function () {
+    var match = self.codeFragment.match(self.syntax);
+    var emptyParameters = true;
+    if(match){
+      emptyParameters = [match[3].trim(), match[4].trim(), match[5].trim()].some(function (element) {
+        return element == '';
+      });
+    }
+
+    if(!match || emptyParameters){
+      return false;
+    } else if (match && emptyParameters){
+      return true;
+    } else {
+      return false
+    }
+  }
+
+  self.getParameters = function () {
+    var match = self.codeFragment.match(self.syntax);
+    if(match){
       return {
         error: false,
         variable: {
           name: match[1].trim(),
-          type: 'uniform',
           min: parseInt(match[3].trim()),
           max: parseInt(match[4].trim()),
           step: parseInt(match[5].trim()),
-          generateCode: uniform.generateCode
         }
       }
     } else {
       return {
         error: true,
-        errorMessage: "The syntax for uniform variable is not correct",
         variable: null
       }
     }
-  },
-  generateCode: function (variableObj) {
-    var vector = []
-    for(var i = variableObj.min; i <= variableObj.max; i+= variableObj.min){
-      vector.push(i);
-    }
-    var vectorName = "vector_" + variableObj.name;
-    var randomName = "random_" + variableObj.name;
-
-    var code = [
-      ["var ", vectorName , "=[", vector, "];"].join(""),
-      ["var ", randomName, "=", "Math.floor((Math.random() * ", vector.length ,"))"].join(""),
-      ["var ", variableObj.name, "=", vectorName, "[", randomName, "];"].join("")
-    ]
-    return code;
   }
+
+  self.generateCode = function () {
+    var variableParameters = self.getParameters();
+    if(variableParameters.error){
+      return {
+        error: true,
+        code: null
+      }
+    }else {
+      var variable = variableParameters.variable;
+      var vector = []
+      for(var i = variable.min; i <= variable.max; i+= variable.min){
+        vector.push(i);
+      }
+      var vectorName = "vector_" + variable.name;
+      var randomName = "random_" + variable.name;
+
+      var code = [
+        ["var ", vectorName , "=[", vector, "];"].join(""),
+        ["var ", randomName, "=", "Math.floor((Math.random() * ", vector.length ,"))"].join(""),
+        ["var ", variable.name, "=", vectorName, "[", randomName, "];"].join("")
+      ]
+      return {
+        error: false,
+        code: code
+      };
+    }
+  }
+
+
 }
-module.exports = uniform;
+Uniform.prototype.identifier = 'U'
+Uniform.prototype.syntax =  /([a-zA-Z\_\-]+[0-9A-Za-z\_\-\$]*)*\s*=\s*(u|U)\[([^\;]+)\;([^\;]+)\;([^\]]+)\]/
+module.exports = Uniform;
