d5e93989baed832acb19e797be86ecacaca08452
Alexander Soto <nickforspeed25@gmail.com>
2015-08-28 00:12:08 -0500

    Fixed indentation spaces

--- dev/js/utils/answers/common-error.js
+++ dev/js/utils/answers/common-error.js
@@ -1,18 +1,18 @@
 class CommonError {
-	constructor() {
-		this.value = "";
-		this.message = "";
-	}
+  constructor() {
+    this.value = "";
+    this.message = "";
+  }
 
-	validate() {
-		return true;
-	}
+  validate() {
+    return true;
+  }
 
-	static createFromResponse(commonErrorJson) {
-		var commonError = new CommonError();
-		commonError.value = commonErrorJson.value;
-		commonError.message = commonErrorJson.message;
-		return commonError;
-	}
+  static createFromResponse(commonErrorJson) {
+    var commonError = new CommonError();
+    commonError.value = commonErrorJson.value;
+    commonError.message = commonErrorJson.message;
+    return commonError;
+  }
 }
 module.exports = CommonError;
