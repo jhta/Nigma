49bc6453602e42e6dfeafc734d0cc4360610beac
alexsotocx <nickforspeed25@gmail.com>
2015-09-30 22:15:44 -0500

    Started work _ created functions

--- dev/js/actions/space/space-actions.js
+++ dev/js/actions/space/space-actions.js
@@ -1,12 +1,31 @@
 const SpaceConstants = require('../../constants/space/space-constants');
 var Dispatcher = require('../../dispatchers/dispatcher');
 var SpaceApi = require('../../api/utils/space');
+var QuestionAPI = require('../../api/utils/question');
 var SpaceActions = {
   previewQuestion(data) {
 
     SpaceApi.preview({
       question: JSON.stringify(data)
     }, (err, data) => {
+      console.log(data);
+      if(data.ok){
+        Dispatcher.dispatch({
+          type: SpaceConstants.PREVIEW_QUESTION,
+          response: data.ok
+        });
+      }
+    });
+
+  },
+  updateQuestionData(data, questionId) {
+    payload = {
+      question: {
+        data: data
+      },
+      questionid: questionid
+    }
+    QuestionAPI.updateQuestionData(data, (err, data) => {
       console.log(data);
       if(data.ok){
         Dispatcher.dispatch({
--- dev/js/api/utils/question.js
+++ dev/js/api/utils/question.js
@@ -4,10 +4,24 @@ const QuestionAPI = {
     create: {
       route: "/folders/:folderid/questions",
       method: API._REQUEST_METHOD.post
+    },
+    updateQuestionData: {
+      route: "/questions/:questionid/data",
+      method: API._REQUEST_METHOD.put
     }
   },
   createQuestion(data, cb){
     const route = this._routes.create;
+    API.callAjaxRequest(route, data, (err, res) => {
+      if(err){
+        cb(true, null);
+      } else {
+        cb(!res.body.ok, res.body.question);
+      }
+    });
+  },
+  updateQuestionData(data, cb){
+    const route = this._routes.updateQuestionData;
     API.callAjaxRequest(route, data, (err, res) => {
       if(err){
         cb(true, null);
