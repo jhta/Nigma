d9f57faa342aff3f011cf539ef0883daa6170259
jhta <jeisonhs93@gmail.com>
2015-10-23 07:57:39 -0500

    api url updated

--- dev/js/api/API.js
+++ dev/js/api/API.js
@@ -1,7 +1,6 @@
 const request = require('superagent');
 const _ = require('lodash');
-//const _URL = "http://104.131.58.229:4000/api";
-const _URL = "http://localhost:4000/api";
+const _URL = "http://104.131.58.229:4000/api";
 
 const Auth = require('../utils/auth');
 
